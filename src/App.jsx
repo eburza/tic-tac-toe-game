dc94b951409e61f7f0a52facc196bc8aa0f807c0
Emilia Burza <emila.burza@gmail.com>
2024-09-22 12:28:40 +0200

    Clean up App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,10 @@
-//import React from 'react'
+import GameStart from './GameStart'
 
-function App() {
+export default function App() {
 
   return (
     <>
-      <section>{`
-        -- New game menu start --
-
-        Pick player 1's mark
-        Remember: X goes first
-
-        New Game (vs CPU)
-        New Game (vs player)
-
-        -- New game menu end --
-
-        -- Game board start --
-
-        -- x/o icon -- turn
-
-        X (You) !-- Your score --
-        Ties !-- Ties score --
-        X (CPU) !-- CPU score --
-
-        Oh no, you lost
-        You won!
-        Player -- 1/2 -- wins!
-
-        x/o icon -- takes the round
-        Round tied
-        Restart game?
-
-        Quit
-        Next round
-        No, cancel
-        Yes, restart
-
-        Game board end`}
-      </section>
-
+      <GameStart />
     </>
   )
 }
-
-export default App
-
-/*
-<body>
-
-  <!-- New game menu start -->
-
-  Pick player 1's mark
-  Remember: X goes first
-
-  New Game (vs CPU)
-  New Game (vs player)
-
-  <!-- New game menu end -->
-
-  <!-- Game board start -->
-
-  <!-- x/o icon --> turn
-
-  X (You) <!-- Your score -->
-  Ties <!-- Ties score -->
-  X (CPU) <!-- CPU score -->
-
-  Oh no, you lost
-  You won!
-  Player <!-- 1/2 --> wins!
-
-  <!-- x/o icon --> takes the round
-  Round tied
-  Restart game?
-
-  Quit
-  Next round
-  No, cancel
-  Yes, restart
-
-  <!-- Game board end -->
-
-</body>
-*/
