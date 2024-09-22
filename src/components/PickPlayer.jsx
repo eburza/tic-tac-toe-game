3b2d06627db6eb225d0feb165320b577cb537037
Emilia Burza <emila.burza@gmail.com>
2024-09-22 12:38:52 +0200

    Create pick player component, no logic added

---
+++ src/components/PickPlayer.jsx
@@ -0,0 +1,10 @@
+
+export default function PickPlayer() {
+    return (
+        <>
+            <h1>Pick player 1&apos;s mark</h1>
+            <div>x / o</div>
+            <h2>Remember: X goes first</h2>
+        </>
+    )
+}
