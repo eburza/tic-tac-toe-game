a7d9357d2fb7e4bbdf053fc4e3f30f21a1388eca
Emilia Burza <emila.burza@gmail.com>
2024-10-05 20:46:55 +0200

    Update player turn test file

--- src/_test_/components/PlayerTurn.test.jsx
+++ src/_test_/components/PlayerTurn.test.jsx
@@ -1,4 +1,4 @@
-import { render, fireEvent, getByRole, getByTestId } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import PlayerTurn from '../../components/PlayerTurn'
 
@@ -8,7 +8,7 @@ describe('player turn', () => {
         expect(getByTestId('player-turn')).toBeInTheDocument()
     })
     test('renders player turn correctly', () => {
-        const {getByAltText} = render(<PlayerTurn id='test-player-turn' isXTurn={true}/>)
+        const {getByAltText} = render(<PlayerTurn id='test-player-turn' />)
         expect(getByAltText('X')).toBeInTheDocument()
     })
 })
