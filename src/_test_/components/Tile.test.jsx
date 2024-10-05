6b634ce43303fe802160ff73ee1803a7eae98f04
Emilia Burza <emila.burza@gmail.com>
2024-10-05 16:52:03 +0200

    Update tile test file

--- src/_test_/components/Tile.test.jsx
+++ src/_test_/components/Tile.test.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom';
-import Tile from '../../components/Tile';
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Tile from '../../components/Tile'
 
 describe('Tile component', () => {
     it('renders correctly', () => {
-      const { getByRole } = render(<Tile id="test-tile" handleClick={() => {}} />);
-      expect(getByRole('button')).toBeInTheDocument();
-    });
-  
+      const { getByRole } = render(<Tile id="test-tile" handleClick={() => {}} />)
+      expect(getByRole('button')).toBeInTheDocument()
+    })
+
     it('displays X when tileContent is X', () => {
-      const { getByAltText } = render(<Tile id="test-tile" handleClick={() => {}} tileContent="X" />);
-      expect(getByAltText('X')).toBeInTheDocument();
-    });
+      const { getByAltText } = render(<Tile id="test-tile" handleClick={() => {}} tileContent="X" />)
+      expect(getByAltText('X')).toBeInTheDocument()
+    })
   
     it('calls handleClick when clicked', () => {
-      const handleClick = jest.fn();
-      const { getByRole } = render(<Tile id="test-tile" handleClick={handleClick} />);
-      fireEvent.click(getByRole('button'));
-      expect(handleClick).toHaveBeenCalled();
-    });
-  });
+      const handleClick = jest.fn()
+      const { getByRole } = render(<Tile id="test-tile" handleClick={handleClick} />)
+      fireEvent.click(getByRole('button'))
+      expect(handleClick).toHaveBeenCalled()
+    })
+  })
