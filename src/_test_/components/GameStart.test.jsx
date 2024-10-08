18d8c0391a9a5bdcc183c7fff6924995a4ae3c57
Emilia Burza <emila.burza@gmail.com>
2024-10-08 10:46:43 +0200

    Add tests to game start test file

--- src/_test_/components/GameStart.test.jsx
+++ src/_test_/components/GameStart.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import { renderWithProvider } from '../utilis/test-utils'
 import { mockGameContext } from '../utilis/test-mockGameContext'
@@ -9,5 +9,23 @@ describe('render game start', () => {
     test('renders correctly', () => {
         const {getByTestId} = render(<GameStart />)
         expect(getByTestId('game-start')).toBeInTheDocument()
+    })
+    test('renders pick player component', () => {
+        const {getByTestId} = render (<GameStart />)
+        expect(getByTestId('pick-player-component')).toBeInTheDocument()
+    })
+    test('renders new game buttons', () => {
+        const {getAllByText} = render (<GameStart />)
+        expect(getAllByText(/new\s+game/i)).toHaveLength(2)
+    })
+    test('new game cpu is functional', () => {
+        const {getByText} = renderWithProvider(<GameStart />, {providerProps: mockGameContext})
+        fireEvent.click(getByText('New Game (vs CPU)'))
+        expect(mockGameContext.onStartGame).toHaveBeenCalledWith(true)
+    })
+    test('new game player is functional', () => {
+        const {getByText} = renderWithProvider(<GameStart />, {providerProps: mockGameContext})
+        fireEvent.click(getByText('New Game (vs player)'))
+        expect(mockGameContext.onStartGame).toHaveBeenCalledWith(true)
     })
 })
