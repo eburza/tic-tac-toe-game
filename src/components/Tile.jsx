25b2b0f0644376569b436e805f87a42420f1c221
Emilia Burza <emila.burza@gmail.com>
2025-01-27 21:14:09 +0100

    Add x and o outline when hoovering over empty tile

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,10 +1,25 @@
 import PropTypes from 'prop-types'
 import playerXtile from '../assets/icon-x.svg'
 import playerOtile from '../assets/icon-o.svg'
+import playerXoutline from '../assets/icon-x-outline.svg'
+import playerOoutline from '../assets/icon-o-outline.svg'
 import { PLAYER_X, PLAYER_O } from '../states/gameConstants'
-
+import { useContext, useState } from 'react'
+import GameContext from '../Context'
 
 export default function Tile({ handleClick, tileContent, id, isHeld, ...props}) {
+    const [isHovered, setIsHovered] = useState(false)
+    const { state } = useContext(GameContext)
+
+    const handleMouseEnter = () => {
+        if (!isHeld) {
+            setIsHovered(true)
+        }
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false)
+    }
 
     return(
         <button 
@@ -12,13 +27,17 @@ export default function Tile({ handleClick, tileContent, id, isHeld, ...props}) 
         className='game-tile'
         onClick={handleClick}
         disabled={isHeld}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
         {...props}>
-                    {
-        tileContent === PLAYER_X ? 
-        <img src={playerXtile} alt={PLAYER_X}/> :
-        tileContent === PLAYER_O ?
-        <img src={playerOtile} alt={PLAYER_O}/> :
-        ''
+        {
+            tileContent === PLAYER_X ? 
+            <img src={playerXtile} alt={PLAYER_X}/> :
+            tileContent === PLAYER_O ?
+            <img src={playerOtile} alt={PLAYER_O}/> :
+            isHovered ?
+            <img src={state.isXTurn ? playerXoutline : playerOoutline} alt={state.isXTurn ? PLAYER_X : PLAYER_O}/> :
+            ''
         }
         </button>
     )
