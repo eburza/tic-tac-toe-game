b825d4f10c3060842080691eceef3e5b6e5a09c4
Emilia Burza <emila.burza@gmail.com>
2025-01-29 12:36:24 +0100

    Update hover state logic

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,13 +1,26 @@
 import PropTypes from 'prop-types'
 import playerXtile from '../assets/icon-x.svg'
 import playerOtile from '../assets/icon-o.svg'
+import playerXoutline from '../assets/icon-x-outline.svg'
+import playerOoutline from '../assets/icon-o-outline.svg'
 import { PLAYER_X, PLAYER_O } from '../states/gameConstants'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import GameContext from '../Context'
 
 export default function Tile({ handleClick, tileContent, id, isHeld, ...props}) {
+    const [isHovered, setIsHovered] = useState(false)
     const { state } = useContext(GameContext)
     
+    const handleMouseEnter = () => {
+        if (!isHeld && !isCpuTurn) {
+            setIsHovered(true)
+        }
+    }
+
+    const handleMouseLeave = () => {
+        setIsHovered(false)
+    }
+
     const isCpuTurn = state.gameCpu && (
         (state.playerX && !state.isXTurn) || 
         (!state.playerX && state.isXTurn)
@@ -19,12 +32,16 @@ export default function Tile({ handleClick, tileContent, id, isHeld, ...props}) 
         className='game-tile'
         onClick={handleClick}
         disabled={isHeld || isCpuTurn}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
         {...props}>
         {
             tileContent === PLAYER_X ? 
             <img src={playerXtile} alt={PLAYER_X}/> :
             tileContent === PLAYER_O ?
             <img src={playerOtile} alt={PLAYER_O}/> :
+            isHovered ?
+            <img src={state.isXTurn ? playerXoutline : playerOoutline} alt={state.isXTurn ? PLAYER_X : PLAYER_O}/> :
             ''
         }
         </button>
